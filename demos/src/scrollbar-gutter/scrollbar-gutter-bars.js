6b4f1483ec6ee2a762a3ad489c612714179feb5d
Bramus <bramus@bram.us>
2022-11-15 11:58:24 +0100

    Scrollbar gutter tests: Track various *vw sized bars

---
+++ demos/src/scrollbar-gutter/scrollbar-gutter-bars.js
@@ -0,0 +1,19 @@
+const updateUI = () => {
+    const $bars = document.querySelectorAll('.bar');
+    $bars.forEach($bar => {
+        const width = Math.round($bar.clientWidth);
+        $bar.querySelector('.size').innerText = width;
+    });
+};
+
+const init = (autoTick = 0) => {
+    updateUI();
+    window.addEventListener('resize', (e) => {
+        updateUI();
+    });
+
+    if (autoTick) {
+        setInterval(updateUI, autoTick);
+    }
+};
+export { init };
